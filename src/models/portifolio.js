253b97f50bc31ab668f5ead963c7f4a9a82ca701
davi-canuto <davicanutogregorio@gmail.com>
2023-09-08 08:41:45 -0300

    improvement: add portifolio model

---
+++ src/models/portifolio.js
@@ -0,0 +1,12 @@
+import mongoose, { Schema } from 'mongoose'
+
+const PortifolioSchema = new mongoose.Schema({
+  personalInfo: { type: Schema.Types.ObjectId, ref: 'PersonalInfo' },
+  projects: { type: Schema.Types.ObjectId, ref: 'Project' },
+  academicInfos: { type: Schema.Types.ObjectId, ref: 'AcademicInfo' },
+  languages: { type: Schema.Types.ObjectId, ref: 'Language' },
+  skills: { type: Schema.Types.ObjectId, ref: 'Skill' },
+  techs: { type: Schema.Types.ObjectId, ref: 'Tech' },
+})
+
+module.exports = mongoose.models.Portifolio || mongoose.model('Portifolio', PortifolioSchema)W
