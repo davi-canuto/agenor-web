1f941092d19bd6076c6f04e8bf39979ae19ab4a8
Wendell P. Barreto <wendellp.barreto@gmail.com>
2023-09-18 18:22:19 -0300

    fix: home load

--- src/pages/[id].js
+++ src/pages/[id].js
@@ -1,48 +1,53 @@
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 
-function Page() {
+const Page = () => {
   const router = useRouter();
-  const { id } = router.query;
-  const [data, setData] = useState({});
-  const [loading, setLoading] = useState(true);
+  const { id, preview } = router.query;
+  const [portfolio, setPortfolio] = useState({});
+  const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  useEffect(() => {
-    if (id) {
-      fetch(`/api/portifolio/${id}`)
-        .then((response) => {
-          if (!response.ok) {
-            throw new Error("Network response was not ok");
-          }
-          return response.json();
-        })
-        .then((data) => {
-          setData(data);
-          setLoading(false);
-        })
-        .catch((error) => {
-          setError(error);
-          setLoading(false);
-        });
+  const load = async () => {
+    try {
+      setLoading(true);
+
+      const portfolioRes = await fetch(`/api/portifolio/${id}`, {
+        headers: {
+          preview: !!preview,
+        },
+      });
+      const { success, data: portfolio } = await portfolioRes.json();
+
+      if (!success) {
+        setError({ message: "Unknown error" });
+        return;
+      }
+
+      setPortfolio(portfolio);
+    } catch (data) {
+      setError(error);
     }
+
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    if (id) load();
   }, [id]);
 
-  if (loading) {
-    return <p>Loading...</p>;
-  }
+  if (loading) return <p>Loading...</p>;
 
-  if (error) {
-    return <p>Error: {error.message}</p>;
-  }
+  if (error) return <p>Error: {error.message}</p>;
 
   return (
     <div>
-      <h1>{data?._id}</h1>
-      <p>{data?.firstName}</p>
-      <p>{data?.lastName}</p>
+      <h1>{portfolio?._id}</h1>
+      <p>{portfolio?.firstName}</p>
+      <p>{portfolio?.lastName}</p>
+      <pre>{JSON.stringify(portfolio, null, 2)}</pre>
     </div>
   );
-}
+};
 
 export default Page;
