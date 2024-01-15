aa04c7b84d38adf806993c1a60e0ff9a83ba25c0
davi-canuto <davicanutogregorio@gmail.com>
2024-01-15 13:49:00 -0300

    feat: improves frontend and works in portfolio display

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,10 +2,11 @@ import React from "react";
 import styles from "./css/Header.module.css";
 
 const Header = ({ portfolioName }) => {
-  const title = `Display.js - ${portfolioName} Portfolio`;
   return (
     <header className={styles.header}>
-      <p>{title}</p>
+      <p className={styles.headerTitle}>
+        Display.js - {portfolioName} Portfolio
+      </p>
     </header>
   );
 };
--- src/components/PortfolioDisplay.js
+++ src/components/PortfolioDisplay.js
@@ -1,22 +1,36 @@
 import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
-// import styles from "./css/PortfolioDisplay.module.css";
+import styles from "./css/PortfolioDisplay.module.css";
 
 const PortfolioDisplay = ({ portfolio }) => {
   return (
-    <div>
+    <div className={styles.wrapper}>
       <Header portfolioName={portfolio?.firstName} />
 
-      <main>
-        <div>
-          <h1>{portfolio?._id}</h1>
-          <p>{portfolio?.firstName}</p>
-          <p>{portfolio?.lastName}</p>
-        </div>
-      </main>
+      <div className={styles.container}>
+        <p>Hello! I am</p>
+        <p>
+          {portfolio?.firstName} {portfolio?.lastName}
+        </p>
+        <p>I am a {portfolio?.occupation}</p>
+      </div>
 
       <Footer />
+
+      <style jsx global>{`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+            sans-serif;
+        }
+        * {
+          box-sizing: border-box;
+        }
+      `}</style>
     </div>
   );
 };
